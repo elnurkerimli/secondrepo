cfc8afdd00cd0db8b02e023766944af531540272
elnur <elnurkerimli2@gmail.com>
2023-05-25 14:54:05 +0300

    initial commit

---
+++ src/Book.java
@@ -0,0 +1,15 @@
+class Book {
+
+    // Member variables of this class
+    public String title;
+    public String author;
+
+    // Constructor of this class
+    Book(String title, String author)
+    {
+
+        // This keyword refers top current instance
+        this.title = title;
+        this.author = author;
+    }
+}
---
+++ src/Library.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+class Library {
+
+    // Reference to refer to list of books.
+    private final List<Book> books;
+
+    // Constructor of this class
+    Library(List<Book> books)
+    {
+
+        // This keyword refers to current instance itself
+        this.books = books;
+    }
+
+    // Method of this class
+    // Getting the list of books
+    public List<Book> getListOfBooksInLibrary()
+    {
+        return books;
+    }
+}
